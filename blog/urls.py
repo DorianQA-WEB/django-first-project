ec7cd8a67c776b68c20555b6d26b8828b8aed3bf
DorianQA-WEB <qa.doriangrey@gmail.com>
2026-02-25 23:19:59 +0500

    urls

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,12 @@
-from django.urls import path
-from .views import home_page_view
+from django.urls import path, re_path
+from blog import views
 
-urlpatterns = [path('', home_page_view, name='home'), ]
+urlpatterns = [
+    path('', views.index, name='home'),
+    path('about/', views.about, kwargs={'name': 'Vladimir', 'age': 29}),
+    re_path(r'^contact/', views.contact),
+    re_path(r'^user/(?P<name>\D+)/(?P<age>\d+)/', views.user),
+    re_path(r'^user/(?P<name>\D+)/', views.user),
+    re_path(r'^user/', views.user)
+]
+
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render
 from django.http import HttpResponse
 
+def index(request):
+    return HttpResponse('<h2>Главная страница</h2>')
 
-def home_page_view(request):
-    return HttpResponse("Hello, World!")
+def about(request, name, age):
+    return HttpResponse(f'''<h2>О сайте</h2>
+                        <p>Имя: {name}</p>
+                        <p>Возраст: {age}</p>
+                        ''')
+
+def contact(request):
+    return HttpResponse('<h2>Контакты</h2>')
+
+def user(request, name='Vladimir', age=29):
+    return HttpResponse(f'<h2>Имя пользователя: {name}, Возраст: {age}</h2>')
