d2719ae0261c7716722be9d7ea1874712cdbc4b3
DorianQA-WEB <qa.doriangrey@gmail.com>
2026-03-06 23:07:19 +0500

    static

--- blog/urls.py
+++ blog/urls.py
@@ -1,17 +1,18 @@
 from django.urls import path, re_path, include
 from blog import views
+from django.views.generic import TemplateView
 
 product_patterns = [
     path('', views.products),
     path('new/', views.new),
     path('top/', views.top),
     path('comments/', views.comments),
-    path('questions/', views.questions)
+    path('questions/', views.questions),
 ]
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('about/', views.about, kwargs={'name': 'Vladimir', 'age': 29}),
+    path('about/', TemplateView.as_view(template_name='blog/about.html', extra_context={'header': 'О сайте'})),
     re_path(r'^contact/', views.contact),
     path('user/', views.user),
     re_path(r'^user/(?P<name>\D+)/', views.user),
