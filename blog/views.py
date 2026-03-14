79e23f1c6c0e6fb200e110954efd36776ea71852
DorianQA-WEB <qa.doriangrey@gmail.com>
2026-03-14 23:01:19 +0500

    Base HTML

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from django.template.response import TemplateResponse
 
 def index(request):
-    return render(request, 'blog/index.html', context={'body': '<h1>Hello Word!<h1>'})
+    return render(request, 'base.html')
 
 def about(request):
     return render(request, 'blog/about.html')
